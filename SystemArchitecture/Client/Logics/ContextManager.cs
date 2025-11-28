ce7da5186865e43dd2be621ebd55e66e79079a4d
usn3 <usn3@txstate.edu>
2025-11-28 16:47:08 -0600

    Added CKKS HE scheme to compare with BFV

--- SystemArchitecture/Client/Logics/ContextManager.cs
+++ SystemArchitecture/Client/Logics/ContextManager.cs
@@ -9,15 +9,26 @@ namespace CDTS_PROJECT.Logics
 		public EncryptionParameters EncryptionParams { get; set; }
 		public SEALContext Context { get; set; }
 		
-		public ContextManager()
-		{
-			EncryptionParams = new EncryptionParameters(SchemeType.BFV);
-			const ulong polyModulusDegree = 2048;
-			EncryptionParams.PolyModulusDegree = polyModulusDegree;
-			EncryptionParams.CoeffModulus = CoeffModulus.BFVDefault(polyModulusDegree);
-			EncryptionParams.PlainModulus = new Modulus(1024);
-			Context = new SEALContext(EncryptionParams);
-		}
-
-	}
+	public ContextManager()
+	{
+		// CHOOSE ENCRYPTION SCHEME 
+		// Comment/Uncomment one section below:
+		
+		// BFV (currently active - integer arithmetic)
+		EncryptionParams = new EncryptionParameters(SchemeType.BFV);
+		const ulong polyModulusDegree = 2048;
+		EncryptionParams.PolyModulusDegree = polyModulusDegree;
+		EncryptionParams.CoeffModulus = CoeffModulus.BFVDefault(polyModulusDegree);
+		EncryptionParams.PlainModulus = new Modulus(1024);
+		Context = new SEALContext(EncryptionParams);
+		
+		/*
+		 CKKS (floating-point arithmetic - uncomment to use)
+		 EncryptionParams = new EncryptionParameters(SchemeType.CKKS);
+		 const ulong polyModulusDegree = 8192;
+		 EncryptionParams.PolyModulusDegree = polyModulusDegree;
+		 EncryptionParams.CoeffModulus = CoeffModulus.Create(polyModulusDegree, new int[] { 60, 40, 40, 60 });
+		 Context = new SEALContext(EncryptionParams);
+		*/
+	}	}
 }
--- SystemArchitecture/Server/Services/ContextManager.cs
+++ SystemArchitecture/Server/Services/ContextManager.cs
@@ -11,17 +11,28 @@ namespace CDTS_PROJECT.Services
 
 	public class ContextManager : IContextManager
 	{
-		public ContextManager()
-		{
-			EncryptionParams = new EncryptionParameters(SchemeType.BFV);
-			const ulong polyModulusDegree = 2048;
-			EncryptionParams.PolyModulusDegree = polyModulusDegree;
-			EncryptionParams.CoeffModulus = CoeffModulus.BFVDefault(polyModulusDegree);
-			EncryptionParams.PlainModulus = new Modulus(1024);
-			Context = new SEALContext(EncryptionParams);
-		}
-
-		public EncryptionParameters EncryptionParams { get; set; }
+	public ContextManager()
+	{
+		// === CHOOSE ENCRYPTION SCHEME ===
+		// Comment/Uncomment one section below:
+		
+		// BFV (currently active - integer arithmetic)
+		EncryptionParams = new EncryptionParameters(SchemeType.BFV);
+		const ulong polyModulusDegree = 2048;
+		EncryptionParams.PolyModulusDegree = polyModulusDegree;
+		EncryptionParams.CoeffModulus = CoeffModulus.BFVDefault(polyModulusDegree);
+		EncryptionParams.PlainModulus = new Modulus(1024);
+		Context = new SEALContext(EncryptionParams);
+		
+		/*
+		 CKKS (floating-point arithmetic - uncomment to use)
+		 EncryptionParams = new EncryptionParameters(SchemeType.CKKS);
+		 const ulong polyModulusDegree = 8192;
+		 EncryptionParams.PolyModulusDegree = polyModulusDegree;
+		 EncryptionParams.CoeffModulus = CoeffModulus.Create(polyModulusDegree, new int[] { 60, 40, 40, 60 });
+		 Context = new SEALContext(EncryptionParams);
+		*/
+	}		public EncryptionParameters EncryptionParams { get; set; }
 		public SEALContext Context { get; set; }
 	}
 }
